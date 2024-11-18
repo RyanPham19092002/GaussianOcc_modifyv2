3dcb0993da4b5e0e0df73cdbd572fe0c1bbea36e
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-11-18 07:01:48 +0000

    Initial commit for this folder

--- lib/gaussian_renderer/__init__.py
+++ lib/gaussian_renderer/__init__.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -13,15 +13,30 @@ import torch
 import math, pdb
 import numpy as np
 
-# no semantic 
-import diff_gaussian_rasterization as diff_3d_gaussian_rasterization
-# semantic 
-import diff_gaussian_rasterization_semantic as diff_3d_gaussian_rasterization_semantic
-
+# no semantic
+# import diff_gaussian_rasterization as diff_3d_gaussian_rasterization
+# # semantic
+# import diff_gaussian_rasterization_semantic as diff_3d_gaussian_rasterization_semantic
+from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
 
 from simple_knn._C import distCUDA2
 from .sh_utils import eval_sh
 from .point_utils import depth_to_normal
+
+
+#--------------------visualize---------------------------
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+def visualize_matrix(matrix, title):
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(matrix.cpu().numpy(), annot=True, fmt=".2f", cmap='viridis')
+    plt.title(title)
+
+    # Lưu hình ảnh vào thư mục 'test'
+    plt.savefig(f'test/{title}.png', bbox_inches='tight')  # Lưu hình ảnh
+    plt.close()
 
 
 def DistCUDA2(fused_point_cloud):
@@ -37,7 +52,7 @@ def camera_intrinsic_fov(intrinsic):
     #计算FOV
     w, h = intrinsic[0][2]*2, intrinsic[1][2]*2
     fx, fy = intrinsic[0][0], intrinsic[1][1]
-    
+
     # Go
     fov_x = np.rad2deg(2 * np.arctan2(w, 2 * fx))
     fov_y = np.rad2deg(2 * np.arctan2(h, 2 * fy))
@@ -46,8 +61,8 @@ def camera_intrinsic_fov(intrinsic):
 
 
 def splatting_render(viewpoint_camera, pc, scaling_modifier = 1.0,
-           override_color = None, white_bg = False, opt = None):
-    
+            override_color = None, white_bg = False, opt = None):
+
     """
     Render the scene.
     Background tensor (bg_color) must be on GPU!
@@ -58,7 +73,7 @@ def splatting_render(viewpoint_camera, pc, scaling_modifier = 1.0,
 
     try:
         screenspace_points.retain_grad()
-        
+
     except:
         pass
 
@@ -69,82 +84,44 @@ def splatting_render(viewpoint_camera, pc, scaling_modifier = 1.0,
     #if min(pc.bg_color.shape) != 0:
     bg_color = torch.tensor([0., 0., 0.]).cuda()
 
-    confidence = pc['confidence']
-    
-    # pdb.set_trace()
-    if not opt.use_semantic:
-        if opt.render_type == '3dgs':
-            raster_settings = diff_3d_gaussian_rasterization.GaussianRasterizationSettings(
+    raster_settings = GaussianRasterizationSettings(
                 image_height=int(viewpoint_camera['image_height']),
                 image_width=int(viewpoint_camera['image_width']),
                 tanfovx=tanfovx,
                 tanfovy=tanfovy,
                 bg = bg_color, #torch.tensor([1., 1., 1.]).cuda() if white_bg else torch.tensor([0., 0., 0.]).cuda(), #bg_color,
                 scale_modifier=scaling_modifier,
-
                 viewmatrix=viewpoint_camera['world_view_transform'], # 外参
-                projmatrix=viewpoint_camera['full_proj_transform'], # 3D -> 2D 
-
-                sh_degree=pc['active_sh_degree'],
+                projmatrix=viewpoint_camera['full_proj_transform'], # 3D -> 2D
+                sh_degree=3,
                 campos=viewpoint_camera['camera_center'],
                 prefiltered=False,
-                debug=False, # False
-                confidence=confidence)
+                debug=False
+    )
+    rasterizer = GaussianRasterizer(raster_settings=raster_settings)
 
-            rasterizer = diff_3d_gaussian_rasterization.GaussianRasterizer(raster_settings=raster_settings)
-
-    else:
-        if opt.render_type == '3dgs':
-            raster_settings = diff_3d_gaussian_rasterization_semantic.GaussianRasterizationSettings(
-                image_height=int(viewpoint_camera['image_height']),
-                image_width=int(viewpoint_camera['image_width']),
-                tanfovx=tanfovx,
-                tanfovy=tanfovy,
-                bg = bg_color, #torch.tensor([1., 1., 1.]).cuda() if white_bg else torch.tensor([0., 0., 0.]).cuda(), #bg_color,
-                scale_modifier=scaling_modifier,
-
-                viewmatrix=viewpoint_camera['world_view_transform'], # 外参
-                projmatrix=viewpoint_camera['full_proj_transform'], # 3D -> 2D 
-
-                sh_degree=pc['active_sh_degree'],
-                campos=viewpoint_camera['camera_center'],
-                prefiltered=False,
-                debug=False,
-                confidence = confidence)
-            
-            rasterizer = diff_3d_gaussian_rasterization_semantic.GaussianRasterizer(raster_settings=raster_settings)
-
-
-    means3D = pc['get_xyz']
+    means3D = pc['get_xyz'].reshape(-1, 3)
     means2D = screenspace_points
-    opacity = pc['get_opacity']
+    if pc['get_opacity'].dim() == 3:
+        opacity = pc['get_opacity'].permute(1, 2, 0).view(-1, 1)
+        colors_precomp = pc['colors_precomp'].permute(1, 2, 0).view(-1, 3)
+        scales = pc['get_scaling'].permute(1, 2, 0).view(-1, 3)
+        rotations = pc['get_rotation'].permute(1, 2, 0).view(-1, 4)
+    elif pc['get_opacity'].dim() == 4:
+        opacity = pc['get_opacity'].permute(0, 2, 3, 1).reshape(-1, 1)
+        scales = pc['get_scaling'].permute(0, 2, 3, 1).reshape(-1, 3)
+        rotations = pc['get_rotation'].permute(0, 2, 3, 1).reshape(-1, 4)
+        colors_precomp = pc['colors_precomp'].permute(0, 2, 3, 1).reshape(-1, 3)
 
-    scales = None
-    rotations = None
     cov3D_precomp = None
 
-    scales = pc['get_scaling']
-    rotations = pc['get_rotation']
 
     shs = None
-    colors_precomp = None
 
-    if 'semantic' in pc.keys():
-        colors_precomp = pc['semantic']
-    else:
-        colors_precomp = None
+    
 
-
-    render_normal = None 
-    render_dist = None 
-    surf_depth = None 
-    surf_normal = None 
-
-    # Rasterize visible Gaussians to image, obtain their radii (on screen).
-    # rasterizer return: color, feature_map, radii, depth, alpha
-    # pdb.set_trace()
-
-    rendered_image, radii, depth, alpha = rasterizer(
+    # rendered_image, radii, depth, alpha = rasterizer(
+    rendered_image, _ = rasterizer(
         means3D = means3D,
         means2D = means2D,
         shs = shs,
@@ -153,17 +130,33 @@ def splatting_render(viewpoint_camera, pc, scaling_modifier = 1.0,
         scales = scales,
         rotations = rotations,
         cov3D_precomp = cov3D_precomp)
-
-
-    return {"render": rendered_image,
-            "viewspace_points": screenspace_points,
-            "visibility_filter" : radii > 0,
-            "radii": radii,
-            "depth": depth,
-            'rend_normal': render_normal,
-            'rend_dist': render_dist,
-            'surf_depth': depth,
-            'surf_normal': surf_normal}
+    # print("---------------------------------------------------------------------------------")
+    # print("rendered_image shape", rendered_image.shape, rendered_image.min(), rendered_image.max())
+    # print("depth shape", depth.shape, depth.min(), depth.max())
+    print("Image Height:", int(viewpoint_camera['image_height']))
+    print("Image Width:", int(viewpoint_camera['image_width']))
+    print("Tangent of FOV X (tanfovx):", tanfovx)
+    print("Camera Position (campos):", viewpoint_camera['camera_center'])
+    print("Camera Position Shape:", viewpoint_camera['camera_center'].shape)
+    print("View Matrix (world_view_transform):", viewpoint_camera['world_view_transform'])
+    print("View Matrix Shape:", viewpoint_camera['world_view_transform'].shape)
+    print("Projection Matrix (full_proj_transform):", viewpoint_camera['full_proj_transform'])
+    print("Projection Matrix Shape:", viewpoint_camera['full_proj_transform'].shape)    
+    # print("means3D shape:", means3D.shape, "min:", means3D.min(), "max:", means3D.max())
+    # print("means2D shape:", means2D.shape, "min:", means2D.min(), "max:", means2D.max())
+    # print("colors_precomp shape:", colors_precomp.shape, "min:", colors_precomp.min(), "max:", colors_precomp.max())
+    # print("opacity shape:", opacity.shape, "min:", opacity.min(), "max:", opacity.max())
+    # print("scales shape:", scales.shape, "min:", scales.min(), "max:", scales.max())
+    # print("rotations shape:", rotations.shape, "min:", rotations.min(), "max:", rotations.max())
+    return {"render": rendered_image}
+            # "viewspace_points": screenspace_points,
+            # "visibility_filter" : radii > 0,
+            # "radii": radii,
+            # "depth": depth}
+            # 'rend_normal': render_normal,
+            # 'rend_dist': render_dist,
+            # 'surf_depth': depth,
+            # 'surf_normal': surf_normal}
 
 
 if __name__ == "__main__":
