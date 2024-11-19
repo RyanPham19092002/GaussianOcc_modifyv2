fd56dd27f0f327bf9b378d60a5c7d840e072e5ca
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-11-19 01:48:17 +0000

    Remove logs folder from tracking and add to .gitignore

--- logs/nusc-sem-gs_all/seg_False_0.02_0.1_0.1_pose_yes_yes_mask_False/type_3dgs_0.0_s_0.2_384_l_self_en_w_0.1_ep_8_f_False_infi_True_cont_True_depth_0.1_80.0_101/standard/code/networks/__init__.py
+++
@@ -1,8 +0,0 @@
-from .resnet_encoder import Encoder_res101, ResnetEncoder
-from .occupancy_decoder_2d import VolumeDecoder
-from .pose_decoder import PoseDecoder
-# from .decoder_2d import *
-# from .GS import *
-# from .utils import *
-from .GS_params_networks import *
-# from .networks import NewCRFDepth
